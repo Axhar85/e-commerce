be7af3235e51069789d13f12673193553b9f7567
Axhar85 <azhararshad_ntu@yahoo.com>
2024-06-21 14:35:59 +0200

    connected with mongodb and postmon

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,33 @@
 console.log("Hello World backend");
+
+//packages
+import path from "path";
+import express from "express";
+import dotenv, { config } from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+//utiles
+
+import connectDB from "./config/db.js";
+
+
+dotenv.config()
+
+const port = process.env.PORT || 5000;
+
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.send("Hello World backend");
+});
+
+app.listen(port, () => console.log(`Server runing on port: ${port}`));
